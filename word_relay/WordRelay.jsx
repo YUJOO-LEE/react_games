7eaca0eaa27dc6fdcd4452cecb6aa28a588df5f1
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-15 19:58:49 +0900

    끝말잇기 제작

--- word_relay/WordRelay.jsx
+++ word_relay/WordRelay.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function WordRelay() {
+  const [Word, setWord] = useState('이유주');
+  const [Value, setValue] = useState('');
+  const [Result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (Value.length > 1 && Word[Word.length - 1] === Value[0]) {
+      setResult('딩동댕');
+      setWord(Value);
+      setValue('');
+    } else {
+      setResult('땡');
+    }
+    inputRef.current.focus();
+  }
+  
+  const inputValue = (e) => {
+    setValue(e.target.value);
+  }
+
   return (
-    <div>WordRelay2</div>
+    <div>
+      <h1>끝말잇기</h1>
+      <p>{Word}</p>
+      <form onSubmit={onSubmit}>
+        <input type="text" ref={inputRef} value={Value} onInput={inputValue} />
+        <button>입력</button>
+      </form>
+      <p>{Result}</p>
+    </div>
   )
 }
 
